001098901dc89cd2a27957777bc60dbe380ae071
Natasha Petrus <hello@natashapetrus.com>
2017-09-03 18:21:07 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -5,3 +5,15 @@ function paragraphSelector(){
 function lastImageSelector(){
   return $('img:last');
 }
+
+function ninjaBabySelector(){
+  return $('#baby-ninja');
+}
+
+function divSelector(){
+  return $('div .pics');
+}
+
+function firstListItem(){
+  
+}
